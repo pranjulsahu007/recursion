6c441b8d87b9c1029b6642b23a539142d2ba1806
pranjulsahu007 <hustlerair300@gmail.com>
2024-12-20 00:16:37 +0530

    reversenum prog

---
+++ reversenumber.java
@@ -0,0 +1,21 @@
+public class reversenumber {
+
+    public static void main(String[] args) 
+    {
+        int k =reverse(635);
+        System.out.println(k);
+    }
+   static int sum=0;
+    static int reverse(int n)
+    {
+        int r=0;
+if(n==0)
+{
+    return 0;
+}
+r=n%10;
+sum=sum*10+r;
+reverse(n/10);
+return sum;
+    }
+}
