0bc2761f85fcad35f9ed479b2686d30141f2872b
pranjulsahu007 <hustlerair300@gmail.com>
2024-12-20 00:41:56 +0530

    later

---
+++ palindrome.java
@@ -0,0 +1,15 @@
+public class palindrome
+{
+    public static void main(String[] args) {
+        
+    }
+    static int rev2(int n)
+    {
+        int digits = (int)(Math.log10(n))+1;
+        return helper(n,digits);
+    }
+    static int helper(int n,int digits)
+    {
+        if(n)
+    }
+}
