2c48033c777ba624d61616ee447d4b7796ab9d75
pranjulsahu007 <hustlerair300@gmail.com>
2024-12-19 13:59:21 +0530

    Create nto1prod.java
    factorial of number

---
+++ nto1prod.java
@@ -0,0 +1,20 @@
+public class nto1prod
+{
+    public static void main(String args[])
+    {
+    int k = fun(5);
+    System.out.println(k);
+    }
+    static int fun(int n)
+    {
+        if(n==0)            
+        {
+            return 1;
+            
+        }
+
+        return n*fun(n-1);
+       
+     
+    }
+}
