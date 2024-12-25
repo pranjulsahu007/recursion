6585dde0f72a51576b96075d75e79d7b62438770
pranjulsahu007 <hustlerair300@gmail.com>
2024-12-25 12:25:26 +0530

    palindrome

--- palindrome.java
+++ palindrome.java
@@ -8,8 +8,18 @@ public class palindrome
         int digits = (int)(Math.log10(n))+1;
         return helper(n,digits);
     }
-    static int helper(int n,int digits)
+    
+    
+    static int sum =0;
+    private static int helper(int n,int digits)
     {
-        if(n)
+        if(n==0)
+        {
+            return 0;
+        }
+        int rem =0;
+    rem=n%10;
+    return rem*(int)(Math.pow(10, digits-1))+helper(n/10, digits-1);
+
     }
 }
